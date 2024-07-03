9601adb332b05443e6f709a89e093a4c148637ed
chineduCoded <chinedujohn17@gmail.com>
2024-07-03 17:52:22 +0100

    Remove unnecessary lib and file

--- app/ipstack.py
+++
@@ -1,9 +0,0 @@
-import requests
-from config import settings
-
-def ip_stack(client_ip: str):
-    ipstack_url = f"http://api.ipstack.com/{client_ip}?access_key={settings.ipstack_api_key}"
-    response = requests.get(ipstack_url)
-    response.raise_for_status()
-
-    return response.json()
